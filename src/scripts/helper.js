0948b0fd520dd634fa1705d8f2fecf32a558f96c
Tarun Sharma <98592314+sharmatarun32@users.noreply.github.com>
2025-08-29 10:47:32 +0530

    Update helper.js

--- src/scripts/helper.js
+++ src/scripts/helper.js
@@ -2,7 +2,7 @@ const getPageAndLimit = (args) => {
   const page = args.page > 0 ? args.page : 1;
   const limit = args.limit > 0 ? args.limit : 10;
   const orderBy = args.orderBy || "DESC";
-  const sortBy = args.sortBy || "row_modified_on";
+  const sortBy = args.sortBy || "id";
   const skip = limit * (page - 1);
   return { skip, limit, page, orderBy, sortBy };
 };
