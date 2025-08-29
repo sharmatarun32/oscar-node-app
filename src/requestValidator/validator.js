bc6ac857e8ac57e6f089db52b7c97af790d45e4b
Tarun Sharma <98592314+sharmatarun32@users.noreply.github.com>
2025-08-29 10:46:11 +0530

    Update validator.js

--- src/requestValidator/validator.js
+++ src/requestValidator/validator.js
@@ -1,14 +1,6 @@
 const Yup = require("yup");
 
 const apiSchema = Yup.object().shape({
-  fromDate: Yup.date()
-    .required("fromDate is required")
-    .typeError("fromDate must be a valid date")
-    .max(Yup.ref("toDate"), "fromDate must be before toDate"),
-  toDate: Yup.date()
-    .required("toDate is required")
-    .typeError("toDate must be a valid date")
-    .min(Yup.ref("fromDate"), "toDate must be after fromDate"),
   page: Yup.number()
     .integer("page must be an integer")
     .positive("page must be a positive number")
